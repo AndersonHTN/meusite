59241b9b8eadfc2f4a1263cb9b58561855e209fc
AndersonHTN <56129719+AndersonHTN@users.noreply.github.com>
2019-11-21 21:06:12 -0300

    Add files via upload

--- javascript/blockchain.js
+++ javascript/blockchain.js
@@ -1,4 +1,4 @@
-var enderecoContrato = "0x53C34f4897a4111f9299a9b734aD1d5c380a8Ce5";
+var enderecoContrato = "0xf07E3AD143af88f1085228dD0cc5cE0a39E6F318";
 var provedor = new ethers.providers.Web3Provider(web3.currentProvider);
 ethereum.enable();
 var signatario = provedor.getSigner();
